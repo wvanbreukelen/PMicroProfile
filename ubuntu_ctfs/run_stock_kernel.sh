7fd078b3552251ce4abf03b59731d33410097d1e
Wiebe van Breukelen <wiebefwvb@gmail.com>
2023-03-01 18:22:51 +0100

    Work.

--- ubuntu_ctfs/run_stock_kernel.sh
+++ ubuntu_ctfs/run_stock_kernel.sh
@@ -1,13 +1,11 @@
-qemu-system-x86_64\
-	-cpu host \
+/home/wiebe/Downloads/qemu-7.2.0/build/qemu-system-x86_64 \
+	-cpu host\
 	-enable-kvm \
-	-smp cores=4 \
+	-smp cores=16 \
 	-drive file=ubuntu.img.qcow2,format=qcow2 \
-	-append "root=/dev/sda5 earlyprintk=serial net.ifnames=0 nokaslr " \
-	-kernel $1/arch/x86/boot/bzImage \
 	-machine pc,nvdimm=on \
 	-m 4G,slots=2,maxmem=32G \
-	-object memory-backend-file,id=mem1,mem-path=./nvdimm0,share=off,pmem=on,size=4G,align=2M \
+    -object memory-backend-file,id=mem1,mem-path=nvdimm0,share=on,pmem=on,size=24G,align=2M \
 	-device nvdimm,memdev=mem1,id=nv1,label-size=256K \
 	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:2222-:22 \
 	-net nic,model=e1000 \
