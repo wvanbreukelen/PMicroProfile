188d0edbc84327c48d4a37a2e54dfdec74779fee
Wiebe van Breukelen <wiebefwvb@gmail.com>
2023-03-07 18:22:41 +0100

    Updated scripts.

--- kernels/linux-5.4.232/include/generated/compile.h
+++ kernels/linux-5.4.232/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 25 */
+/* This file is auto generated, version 65 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#25 SMP Tue Feb 28 15:03:18 CET 2023"
+#define UTS_VERSION "#65 SMP Tue Mar 7 15:11:44 CET 2023"
 #define LINUX_COMPILE_BY "wiebe"
 #define LINUX_COMPILE_HOST "wiebe-linux"
 #define LINUX_COMPILER "gcc version 11.3.0 (Ubuntu 11.3.0-1ubuntu1~22.04)"
