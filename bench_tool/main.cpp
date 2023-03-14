0a2de52e25b32098d091872305759a18f552a2ed
Wiebe van Breukelen <wiebefwvb@gmail.com>
2023-03-14 14:22:53 +0100

    Added --mpmem-ops toggle.

--- bench_tool/main.cpp
+++ bench_tool/main.cpp
@@ -14,12 +14,14 @@ int main(int argc, char** argv)
     size_t number_of_files = 0;
     size_t stride_size = 0;
     bool is_verbose = false;
+    bool use_pmem_ops = false;
 
     app.add_option("-f,--path", pmem_path, "Path to PMEM (e.g. /mnt/pmem/bench)")->required();
     app.add_option("-s,--file-size", file_size, "File size")->default_val(4096 * 8);
     app.add_option("-n,--nr-files", number_of_files, "Number of files")->default_val(1);
     app.add_option("--stride-size", stride_size, "Stride size")->default_val(64);
     app.add_flag("-v,--verbose", is_verbose, "Enable verbose output")->default_val(false);
+    app.add_flag("-p,--pmem-ops", use_pmem_ops, "Use pread/pwrite")->default_val(false);
 
     CLI11_PARSE(app, argc, argv);
 
@@ -52,8 +54,8 @@ int main(int argc, char** argv)
 	if (is_verbose)
 		std::cout << "File: " << file_path << std::endl;
 
-        bm.run_bench_file_seq(std::cout, file_path, IOOperation::WRITE, file_size);
-        bm.run_bench_file_seq(std::cout, file_path, IOOperation::READ, file_size);
+        bm.run_bench_file_seq(std::cout, file_path, IOOperation::WRITE, file_size, use_pmem_ops);
+        bm.run_bench_file_seq(std::cout, file_path, IOOperation::READ, file_size, use_pmem_ops);
     }
 
 
