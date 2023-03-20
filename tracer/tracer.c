64b0f973cd435fb36203c622d4b2c0110b802fc5
Wiebe van Breukelen <wiebefwvb@gmail.com>
2023-03-20 17:36:41 +0100

    Work on tracer.

--- tracer/tracer.c
+++ tracer/tracer.c
@@ -10,11 +10,20 @@
 #include <pthread.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <inttypes.h>
 
 #define DEBUG
 #define ND_CMD_TRACE 11
 #define ND_CMD_TRACE_DISABLE 12
 
+#define CURRENT_TRACER "/sys/kernel/debug/tracing/current_tracer"
+
+#define TRACER_BUF_SIZE 128000
+#define TRACER_BUF_SIZE_PATH "/sys/kernel/debug/tracing/buffer_size_kb"
+
+
+//#define ENABLE_SAMPLING
 const int SAMPLE_RATE = 10; // 5 hz
 
 volatile bool is_tracing = true;
@@ -28,6 +37,24 @@ struct sample_thread_args {
 	int fd;
 };
 
+bool is_mmiotrace_enabled() {
+	char tracer[256];
+	FILE *fp = fopen(CURRENT_TRACER, "r");
+	if (fp == NULL) {
+		perror("Error opening current_tracer file");
+		exit(EXIT_FAILURE);
+	}
+	fgets(tracer, sizeof(tracer), fp);
+	fclose(fp);
+	tracer[strcspn(tracer, "\n")] = 0; // remove newline character
+
+	#ifdef DEBUG
+	printf("Current tracer: %s\n", tracer);
+	#endif
+
+	return (strcmp(tracer, "mmiotrace") == 0);
+}
+
 void enable_pmemtrace()
 {
 	#ifndef DEBUG
@@ -38,12 +65,50 @@ void enable_pmemtrace()
 
 void disable_pmemtrace() {
 	// printf("Disabling pmemtrace...\n");
-	
-	
-
 	#ifndef DEBUG
 	ioctl(fd, ND_CMD_TRACE_DISABLE);
 	#endif
+}
+
+unsigned int get_trace_buf_size() {
+	char buf[32];
+
+	int fd = open(TRACER_BUF_SIZE_PATH, O_RDONLY);
+    if (fd < 0) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (read(fd, buf, sizeof(buf)) < 0) {
+        perror("Failed to read file");
+        exit(EXIT_FAILURE);
+    }
+
+	close(fd);
+
+	uintmax_t buf_size = strtoumax(buf, NULL, 10);
+
+	if (buf_size == UINTMAX_MAX && errno == ERANGE) {
+		perror("Error reading current buffer size!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return (unsigned int) buf_size;
+}
+
+void set_trace_buf_size(const unsigned int buf_size) {
+	int fd = open(TRACER_BUF_SIZE_PATH, O_WRONLY);
+    if (fd < 0) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (dprintf(fd, "%d", (int) buf_size) < 0) {
+        perror("Failed to write to file");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
 }
 
 void* pmem_sampler(void *arg)
@@ -85,12 +150,13 @@ void signal_handler(int sig)
 {
 	if (sig == SIGINT) {
 		is_tracing = false;
+		putchar('\n');
 		//signal(SIGINT, SIG_DFL);
 		//raise(SIGINT);
 
-		//if (exec_pid) {
-			//kill(exec_pid, SIGTERM);
-		//}
+		if (exec_pid) {
+			kill(exec_pid, SIGTERM);
+		}
 	}
 }
 
@@ -99,24 +165,40 @@ int main(int argc, char** argv)
 
 
 	if (geteuid() != 0) {
-		printf("This program must be run as root.\n");
+		fprintf(stderr, "This program must be run as root.\n");
 		exit(EXIT_FAILURE);
 	}
 
 	if (argc < 2) {
-		printf("Please provide a ndctl device handle, e.g. /dev/ndctl0\n");
+		fprintf(stderr, "Please provide a ndctl device handle, e.g. /dev/ndctl0\n");
 		exit(EXIT_FAILURE);
 	}
 
 	if (argc < 3) {
-		printf("Please provide the command to run\n");
+		fprintf(stderr, "Please provide the command to run\n");
 		exit(EXIT_FAILURE);
 	}
+
+	if (is_mmiotrace_enabled()) {
+		printf("mmiotrace is enabled\n");
+   	} else {
+		fprintf(stderr, "mmiotrace is not enabled\n");
+
+		exit(EXIT_FAILURE);
+  	}
+
+	if (get_trace_buf_size() < TRACER_BUF_SIZE) {
+		printf("Increasing tracing buffer size to %u bytes...\n", TRACER_BUF_SIZE);
+		set_trace_buf_size(TRACER_BUF_SIZE);
+		sleep(2);
+	}
+
+	//printf("Current trace buffer size: %u\n", get_trace_buf_size());
 
 	int fd = open(argv[1], O_RDWR);
 	if (fd < 0)
 	{
-		printf("Failed to open device %s!\n", argv[1]);
+		fprintf(stderr, "Failed to open device %s!\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -129,11 +211,13 @@ int main(int argc, char** argv)
     sigaddset(&sa.sa_mask, SIGINT);
     sigaction(SIGINT, &sa, NULL);
 
-	struct sample_thread_args thread_args = {SAMPLE_RATE, fd};
-	
 	enable_pmemtrace();
 
 	pthread_t tid;
+
+
+	#ifdef ENABLE_SAMPLING
+	struct sample_thread_args thread_args = {SAMPLE_RATE, fd};
 
 	if (pthread_create(&tid, NULL, pmem_sampler, (void*) &thread_args) < 0) {
 		fprintf(stderr, "Error: pthread_create failed!\n");
@@ -142,6 +226,7 @@ int main(int argc, char** argv)
 
 	pthread_detach(tid);
 	pthread_join(tid, NULL);
+	#endif
 
 
 	int ret_code, status;
@@ -160,7 +245,10 @@ int main(int argc, char** argv)
 	
 	printf("Disabling pmemtrace...\n");
 
+	#ifdef ENABLE_SAMPLING
 	pthread_cancel(tid);
+	#endif
+
 	disable_pmemtrace();
 
 	return ret_code;
