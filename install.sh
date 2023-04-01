8aba549342cc3051e83802c1e45c46213ad8115b
Wiebe van Breukelen <wiebefwvb@gmail.com>
2023-04-01 17:31:52 +0200

    Prettify install script.

--- install.sh
+++ install.sh
@@ -2,41 +2,59 @@
 
 cwd=$(pwd)
 
+function run_command {
+    local cmd="$1"
+    local msg="$2"
+    # if [ ${#msg} -gt 0 ]; then
+    #     echo -e "\e[32m${msg}...\e[0m"
+    # fi
+    if $cmd > /dev/null 2>&1; then
+        if [ ${#msg} -gt 0 ]; then
+            echo -e "\e[32m[✔] Successfully ${msg}!\e[0m"
+        fi
+    else
+        echo -e "\e[31m${msg} failed! Printing output:\e[0m"
+        $cmd
+        exit 1
+    fi
+}
+
+
 # WIP: do kernel install...
 
 echo "Installing Apache Arrow..."
 
-sudo apt update
-sudo apt install -y -V ca-certificates lsb-release wget
-rm -f apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt update
-sudo apt install -y -V libarrow-dev # For C++
-sudo apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
-sudo apt install -y -V libparquet-dev # For Apache Parquet C++
+run_command "sudo apt update" ""
+run_command "sudo apt install -y -V ca-certificates lsb-release wget" ""
+run_command "rm -f apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb" ""
+run_command "wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb" ""
+run_command "sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb" "installed Apache Arrow package"
+run_command "sudo apt update" ""
+run_command "sudo apt install -y -V libarrow-dev" "installed libarrow-dev"
+run_command "sudo apt install -y -V libarrow-dataset-dev" "installed libarrow-dataset-dev"
+run_command "sudo apt install -y -V libparquet-dev" "installed libparquet-dev"
 
 echo "Building pmemtrace"
 
-rm -rf pmemtrace/build
-mkdir pmemtrace/build
+run_command "rm -rf pmemtrace/build" ""
+run_command "mkdir pmemtrace/build" ""
 cd pmemtrace/build
-cmake ..
-make -j4
+run_command "cmake .." ""
+run_command "make -j4" "built pmemtrace"
 
 cd $cwd
 
 echo "Building pmemreplay"
 
-rm -rf pmemreplay/build
-mkdir pmemreplay/build
+run_command "rm -rf pmemreplay/build" ""
+run_command "mkdir pmemreplay/build" ""
 cd pmemreplay/build
-cmake ..
-make -j4
+run_command "cmake .." ""
+run_command "make -j4" "built pmemreplay"
+
+cd $cwd
 
 echo "Cleaning..."
-rm -f apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+run_command "rm -f apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb" ""
 
 echo "Done!"
-
-
